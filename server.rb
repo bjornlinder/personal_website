0efdd0bddcff141811bcf79266451e6b416bb82f
Bjorn Linder <bjornishungry@hotmail.com>
2014-03-03 13:52:30 -0500

    adding the real files

--- server.rb
+++ server.rb
@@ -1,27 +1,21 @@
 require 'sinatra'
-require 'pry'
 
 set :views, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
 
-#<a href='#{link.downcase.gsub}'> link 
-
 get '/blog/:article' do
-  @path=params[:path]
+  @path=params[:article]
   @date="On Jan 1, 2014"
   def format_text_html (text)
-    text.gsub(/\'/, '&#39;').insert(0,"<p>").gsub("\n",'<p></p>')
+    text.gsub(/\'/, '&#39;').insert(0,"<p>").gsub("\n\n",'</p><p>')
   end
-  content=File.read(File.join('public', 'pre-work.html'))
-  #class BlogPost def initialize (title,page_title,header,date,image_path)
-  #CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-  #  member = Members_struct.new(index_value,row[0],row[1],row[2],row[3])
-  #Blog_data=Struct.new(:title, :page_title, :header, :date, :image_path)  
-  @blog_data = []
-  @blog_data << ["pre-work", "Blog / Life, the Universe and Everything", "Launch Academy: Reflection on Pre-Work", "On Jan 1, 2014", "images/comic.gif"]
+  content=File.read(File.join('public', @path.to_s))
 
-  #pre_work=Blog_data.new("pre-work", "Blog / Life, the Universe and Everything", "Launch Academy: Reflection on Pre-Work", "On Jan 1, 2014")
-    
+  @blog_data = {}
+  @blog_data["pre-work.html"] = ["Launch Academy: Reflection on Pre-Work", "On Feb 16, 2014"]
+  @blog_data["first-post.html"] = ["First Blog Post", "Jan 1, 2014", "images/comic.gif"]
+  @blog_data["launch-academy-week-1.html"] = ["Launch Academy : Week 1", "Feb 22, 2014", "images/sudo-sandwich.png"]
+  
   @text = format_text_html (content.to_s)
 
   erb :blog_post, :layout => :layout
--- testing_server.rb
+++ testing_server.rb
@@ -16,3 +16,8 @@ end
   <input type = "text" name = "comment"></input>
   <input type = "submit" name = "submit" value = "submit"></input>
 </form>'
+
+#class BlogPost def initialize (title,page_title,header,date,image_path)
+#CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
+#  member = Members_struct.new(index_value,row[0],row[1],row[2],row[3])
+#Blog_data=Struct.new(:title, :page_title, :header, :date, :image_path)  
