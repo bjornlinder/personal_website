600bc3dd9125ea027e2eb0ef9ce7cb9f85af93af
Bjorn Linder <bjornishungry@hotmail.com>
2014-03-04 16:40:44 -0500

    fixed footer formatting, added get /, starting on treasure-hunter

---
+++ public/js/treasurehunter.js
@@ -0,0 +1,14 @@
+(function(a,b,c){var name=prompt("Hey there baby, what's your name?");
+var number=prompt("Yo girl, gimme your number. Cause uh, I lost mine.");
+    ;return name}
+
+function()
+alert("Awesome! I'll call you sometime, k?");
+} else {
+	alert("That's the number for a suicide help hotline, isn't it?")
+}
+
+$('#game').displayTile({
+    alert("Awesome! I'll call you sometime, k?");
+	//left: '-195px'
+});
--- server.rb
+++ server.rb
@@ -1,15 +1,18 @@
 require 'sinatra'
+require_relative 'treasure-hunter'
+require 'pry'
 
 set :views, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/blog/:article' do
-  @path=params[:article]
-  @date="On Jan 1, 2014"
+  @path = params[:article]
+  
   def format_text_html (text)
     text.gsub(/\'/, '&#39;').insert(0,"<p>").gsub("\n\n",'</p><p>')
   end
-  content=File.read(File.join('public', @path.to_s))
+  content = File.read(File.join('public', @path.to_s))
+  @text = format_text_html (content.to_s)
 
   @blog_data = {}
   @blog_data["pre-work.html"] = ["Launch Academy: Reflection on Pre-Work", "On Feb 16, 2014"]
@@ -18,18 +21,21 @@ get '/blog/:article' do
   @blog_data["project-updates.html"] = ["Programming Updates - Sinatra & Other Projects
 ", "Mar 3, 2014"]
   
-  @text = format_text_html (content.to_s)
   erb :blog_post, :layout => :layout
 end
 
 get '/:path' do
+  #binding.pry
   @path=params[:path].chomp('.html')
   @title=@path.gsub(/'-'/,' ').split.map(&:capitalize).join(' ')
 
- begin
-    erb @path.to_sym, :layout => :layout
- rescue Errno::ENOENT
-   erb :index
- end 
+  begin
+      erb @path.to_sym, :layout => :layout
+  rescue Errno::ENOENT
+     erb :index
+  end 
 end
 
+get '/' do
+  erb :index
+end
