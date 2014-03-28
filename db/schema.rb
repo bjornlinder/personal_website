192ebc94dbc874089426a1d0b3b1718a59e34482
Bjorn Linder <bjornishungry@hotmail.com>
2014-03-28 15:37:21 -0400

    fixed error in csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324223140) do
+ActiveRecord::Schema.define(version: 20140322190932) do
 
   create_table "comments", force: true do |t|
     t.string   "name",       null: false
@@ -22,14 +22,12 @@ ActiveRecord::Schema.define(version: 20140324223140) do
   end
 
   create_table "posts", force: true do |t|
-    t.string   "title",                         null: false
+    t.string   "title",      null: false
     t.string   "media"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "path"
-    t.boolean  "allow_comments", default: true
-    t.text     "preview_text"
   end
 
 end
