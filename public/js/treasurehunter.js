9625e030aa00896aabca1a67d5051865d2b41db1
Bjorn Linder <bjornishungry@hotmail.com>
2014-04-12 18:04:42 -0400

    Added treasure hunter game. Also cleaned up site navigation.

--- public/js/treasurehunter.js
+++ public/js/treasurehunter.js
@@ -11,15 +11,12 @@
 
 //tiles change color on hover
 
-$('#game-tile img').hover( function() {
-    this.src = 'http://i.imgur.com/nTj3Fxx.gif'
-}, function() {
-    this.src = 'http://i.imgur.com/90Mmdcm.png'
-})
+// $('game-grid.game-tile').hover( function() {
+//     this.src = 'http://i.imgur.com/nTj3Fxx.gif'
+// }, function() {
+//     this.src = 'http://i.imgur.com/90Mmdcm.png'
+// })
 
-function result(output){
-    console.log(output);
-}
 
 // $('#result') ( function() {
 // alert("Awesome! I'll call you sometime, k?");
@@ -43,19 +40,86 @@ function result(output){
 // 		
 // 		$('#move-button')
 // 		$('flower-description').load('fragments/lilies.html');
-// 		
-// 		
 // 	});
 // });
 
-function North() {
-	alert("Moving north.");
-	return 'move north';
-	location[0]
+ playerLocation = [0,0,0]; // Current playerLocation
+// 
+var validMoves = ["north","south","east","west","dig"];
+
+function move(dir) {
+  if ($.inArray(dir, validMoves) == -1) {
+    alert("DOES NOT COMPUTE. REPORT CODE 00000000001011101111 TO ADMINISTRATOR.");
+    return;
+  }
+  
+  if ((playerLocation[0] == 0 && dir == 'north') || (playerLocation[1] == 0 && dir == 'west') || (playerLocation[1] == 3 && dir == 'east') || (playerLocation[0] == 3 && dir == 'south')) {
+    alert("You have reached the edge of the world. You barely catch youself before falling off.");
+    return;
+  }
+     // >= @dimensions[0]||@playerLocation[0]<0||@playerLocation[1]<0||@playerLocation[1]>=@dimensions[1])
+  if (dir == "dig") {
+    dig(playerLocation[2]);
+  }
+  else {
+   playerLocation[2] = 0;
+    if (dir == "north") {
+      playerLocation[0] -= 1 }
+    else if (dir == "south") {
+      playerLocation[0] += 1 }
+    else if (dir == "east") {
+      playerLocation[1] += 1 }
+    else if (dir == "west") {
+      playerLocation[1] -= 1 }
+
+    updateGrid();
+  }
 }
 
-var north = document.getElementById('north');
-north.onclick = North;
+function updateGrid() {
+  var column = "div.game-column." + playerLocation[0]
+  var row = "div.game-row." + playerLocation[1]
+  
+  $(".game-player").toggleClass("game-player");
+  $(column).find(row).toggleClass("game-player");
+}
 
-var location = [0,0,0];
+$(":button").click(function() {
+  move(this.id)
+});
+
+updateGrid();
+
+function dig(depth) {
+  console.log(playerLocation)
+  console.log(dug)
+  if (playerLocation[2] == 3) {
+    alert("You find hard rock and can't dig any deeper. Try moving.")
+  }
+  else if (dug[playerLocation]) {
+    playerLocation[2] += 1;
+    dig(playerLocation[2]);
+  }
+  else {
+    var result;
+    dug[playerLocation] = true;
+    $.get('/dig', function(data) { 
+      console.log("request returned" + data);
+      var result = data;
+      var formatTreasure = "<li>" + result + "</li>"
+      $('#current-treasure').text(data);
+      if (data != "You found some crap. No really, it seems a dog conducted some business and cleaned up after itself." && data != "You find nothing.") {
+        $("#treasure").append(formatTreasure);
+      }
+    });
+  }
+}
+
+dug = {};
+// 
+// // var north = document.getElementById('north');
+// // north.onclick = North;
+// 
+// // var playerLocation = [0,0,0];
+// return false;
 
--- server.rb
+++ server.rb
@@ -5,10 +5,23 @@ require_relative 'environments'
 require_relative 'blog'
 require_relative 'models/comment'
 require_relative 'models/post'
+require 'pry'
 
 set :views, File.dirname(__FILE__) + '/views'
 set :models, File.dirname(__FILE__) + '/models'
 set :public_folder, File.dirname(__FILE__) + '/public'
+
+
+get '/:treasure-hunter' do
+	@game = Game.new
+  @treasure = []
+  erb "treasure-hunter".to_sym, layout: :layout
+end
+
+get '/dig' do  
+  treasure = Game.treasure(params['d'].to_i)  
+  return treasure
+end
 
 get '/blog/:article' do
   @path = params[:article]
@@ -21,7 +34,6 @@ get '/blog/:article' do
 		@post = Post.find_by(path: @path)
 	  content = File.read(File.join('public', @path.to_s))
 	  @text = format_text_html (content.to_s)
-		#@comments_enabled = @post.comments_enabled
 		@post_comments = Comment.where(post_id: @post.id).all
 	rescue ActiveRecord::RecordNotFound => b
 	rescue Errno::ENOENT => c
@@ -34,22 +46,15 @@ end
 get '/:path' do
   @path=params[:path].chomp('.html')
   @title=@path.gsub(/'-'/,' ').split.map(&:capitalize).join(' ')
-
   begin
-      erb @path.to_sym, :layout => :layout
+    erb @path.to_sym, :layout => :layout
   rescue Errno::ENOENT
-     erb :index
+    erb :index
   end 
 end
 
 get '/' do
   erb :index
-end
-
-get '/treasure-hunter' do
-	@@games = {}
-	#create grid in ruby, save grid in javascript
-	#@grid
 end
 
 post '/blog/:post_id/comments' do
